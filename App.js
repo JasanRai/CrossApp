bdece4ebe1cd76bf5d3dec96f868b7af217737ff
Jasan Rai <jasanrai@Jasans-MacBook-Air.local>
2020-08-22 16:02:00 +1000

    adding function complete

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{Component} from 'react';
-import { StyleSheet, Text, View, Button, SafeAreaView, FlatList, TextInput} from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView, FlatList, TextInput, TouchableOpacity, TouchableWithoutFeedback} from 'react-native';
 // custom component
 import {TextBox} from './component/TextBox';
 
@@ -12,30 +12,35 @@ state ={
   expenseCategory: ''
 }
 
-listData = [
-  { id: '1', amount : 50, category:'food'},
-  { id: '2', amount : 150, category:'groceries'},
-  { id: '3', amount : 30, category:'transport'},
-]
+listData = []
+
   render() {
   return (
     <SafeAreaView>
-      <Text>Add your expense</Text>
-      <TextInput
-      style={styles.input}
-        placeholder="$ amount"
-        onChangeText={ text =>this.setState({expenseAmount: parseFloat (text)}) }
-        keyboardType="number-pad" />
-      <TextInput
-      style={styles.input}
-      placeholder="category"
-      onChangeText={ text =>this.setState({ expenseCategory: text})} 
-      />
-      <Button title="Add" onPress={this.addItem()} />
+      <View style ={styles.main}>
+        <Text>Add your expense</Text>
+        <TextInput
+        style={styles.input}
+        placeholder="$ Amount"
+        onChangeText={text =>this.setState({expenseAmount: parseFloat(text) })}
+        keyboardType= "number-pad"/>
+        <TextInput
+        style={styles.input}
+        placeholder="Category"
+        onChangeText={text => this.setState({expenseCategory: text }) }
+        />
+      </View>
+      
+      <View>
+        <TouchableOpacity style ={styles.button} onPress = {this.addItem}>
+          <Text style = {styles.buttonText}>Add</Text>
+        </TouchableOpacity>
+      </View>
+
       <FlatList
         data={this.listData}
         renderItem={this.renderList}
-        keyExtractor={expense => expense.id}
+        keyExtractor={item=> item.id}
         extraData={this.state.expenseAmount}
       />
       
@@ -46,30 +51,40 @@ listData = [
     <Item amount={item.amount} category={item.category} />
   )
   addItem = () => {
-    if(isNaN(this.state.expenseAmount) || this.state.expenseAmount == 0)
+    if(isNaN(this.state.expenseAmount) || this.state.expenseAmount == 0 || this.state.expenseCategory == '')
     {
       return;
     }
       let id = new Date().getTime().toString()
-      let item = {
+      let listItem = {
         id: id,
         amount: this.state.expenseAmount,
         category: this.state.expenseCategory
       }
-      this.listData.push(item)
+      this.listData.push(listItem)
+      console.log('adding')
+      this.setState({expenseAmount:0})
       // this.setState({expenseAmount:0})
   }
 }
 
+const colors ={
+  primary : 'hsla(330, 38%, 65%, 1)'
+}
+
 const styles = StyleSheet.create({
-  main: {
-  paddingHorizontal : 10,
-  },
   input: {
-    width: '100%',
-    padding: 10,
-    borderColor: 'black',
-    borderWidth: 1,
-    marginVertical : 15
+  paddingHorizontal : 10,
+  borderColor:'black',
+  borderWidth: 1,
+  marginVertical : 15
+  },
+  button:{
+    padding: 15,
+    backgroundColor: colors.primary
+  },
+  buttonText:{
+    color: 'white',
+    textAlign: 'center'
   }
 });
