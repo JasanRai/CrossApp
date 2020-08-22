23c38a76a29855fc6dff7782ca57fb104be97a21
Jasan Rai <jasanrai@Jasans-MacBook-Air.local>
2020-08-22 22:05:06 +1000

    adding delete icon

--- component/Item.js
+++ component/Item.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity, Image} from 'react-native';
 
 export const Item = (props) => {
     return(
         <View style ={itemStyles.item}>
-            <Text style={itemStyles.text}>{props.category}</Text>
-            <Text style={itemStyles.text}>{props.amount}</Text>
+            <View style = {itemStyles.row}>
+                <Text style={itemStyles.text}>{props.category}</Text>
+                <Text style={itemStyles.text}>{props.amount}</Text>
+            </View>
+            <TouchableOpacity>
+                <Image style ={itemStyles.icon} source={require('../assets/times-circle-solid.png')} />
+            </TouchableOpacity>
         </View>
     )
 }
@@ -20,5 +25,15 @@ const itemStyles = StyleSheet.create({
     text: {
         fontSize: 16,
         color: 'black'
+    },
+    row: {
+        flexDirection: 'row',
+        flex: 1,
+        justifyContent: 'space-between',
+        paddingRight: 10,
+    },
+    icon: {
+        width: 25,
+        height: 25
     }
 })
